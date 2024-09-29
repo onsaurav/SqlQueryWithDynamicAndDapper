b49b4bef3107ef3031aa5b20bc6001c26b9f1f15
onsaurav <onsaurav@hotmail.com>
2024-09-29 20:54:24 +0600

    Add project files.

---
+++ SimpleSqlQuery/Program.cs
@@ -0,0 +1,15 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+Console.WriteLine("Simple query with Dynamic C#!");
+
+
+string connectionString = "Server=.;Database=MovieClub;User Id=kcbdbuser;Password=kcbdbuser;TrustServerCertificate=True;";
+
+using var con = new SqlConnection(connectionString);
+
+IEnumerable<dynamic> movies = con.Query("SELECT * FROM Movie");
+foreach (dynamic item in movies)
+{
+    Console.WriteLine($"Id: {item.Id}, Name: {item.Name}, Genre: {item.Genre}, Rating: {item.Rating}, Language: {item.Language}");
+}
